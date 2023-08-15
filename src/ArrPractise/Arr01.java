432758692e983b72eff57abdf89a7ce3ab6133a6
BinduShivaK <kbshiva009@gmail.com>
2023-08-15 10:32:26 -0500

    added files

---
+++ src/ArrPractise/Arr01.java
@@ -0,0 +1,10 @@
+package ArrPractise;
+
+public class Arr01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kbsss
+ *
+ */
+module Student {
+}
